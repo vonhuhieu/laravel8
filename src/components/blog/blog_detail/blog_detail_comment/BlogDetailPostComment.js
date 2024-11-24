db438ab6a1f7fc3c7a82d1e4ef73c10e01dad93e
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-24 07:03:19 +0700

    Viết lại chức năng nhập và hiển thị bình luận theo thời gian thực. Đồng thời khi reload lại trang thì dữ liệu không bị sai

--- src/components/blog/blog_detail/blog_detail_comment/BlogDetailPostComment.js
+++ src/components/blog/blog_detail/blog_detail_comment/BlogDetailPostComment.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import Validate from "../../../validate/Validate.js";
 import { useParams } from "react-router-dom";
-import ApiBlogDetailComment from "../../../../api/blog/blog_detail/ApiBlogDetailComment";
-import BlogDetailComment from "./BlogDetailComment.js";
+import ApiBlogDetailComment from "../../../../api/blog/blog_detail/ApiBlogDetailComment.js";
 
 const BlogDetailPostComment = (props) => {
+    // các biến khác
+    const baseUrl = `http://127.0.0.1:8000/upload/user/avatar/`;
+
     // params
     const params = useParams();
     const id_blog = params.blog_id;
@@ -32,7 +34,7 @@ const BlogDetailPostComment = (props) => {
         let errorsSubmit = {};
         if (comment === "") {
             check_validate = false;
-            errorsSubmit.comment = "Please type your comment";
+            errorsSubmit.notComment = "Please type your comment";
         }
         if (!isAuthenticated) {
             check_validate = false;
@@ -44,30 +46,67 @@ const BlogDetailPostComment = (props) => {
             let response = await ApiBlogDetailComment(id_blog, id_user, name_user, id_comment, comment, image_user);
             if (response && response.status === 200) {
                 if (response.data) {
-                    setListComments((prevComments) => ([
-                        ...prevComments, response.data 
-                    ]));
-                    // Tăng số lượng bình luận (callback từ cha)
-                    props.incrementCountComments();
-                    // Xóa nội dung ô nhập bình luận
                     setComment("");
+                    props.setListComments((prevComments) => ([
+                        ...prevComments, response.data
+                    ]));
+                    setListComments((prevComments) => ([
+                        ...prevComments, response.data
+                    ]));
                 }
+                else {
+                    console.log("No data from api");
+                }
+            }
+            else if (response.status !== 200) {
+                console.log("Error");
+            }
+            else {
+                console.log("No data from server");
             }
         }
     };
+
+    const renderComments = () => {
+        if (listComments && listComments.length > 0) {
+            return listComments.map((value, key) => {
+                return (
+                    <li className="media" key={`comment-${value}`}>
+
+                        <a className="pull-left" href="#">
+                            <img style={{ width: "121px", height: "86px" }} className="media-object" src={`${baseUrl}${value.image_user}`} alt="" />
+                        </a>
+                        <div className="media-body">
+                            <ul className="sinlge-post-meta">
+                                <li><i className="fa fa-user">{value.name_user}</i></li>
+                                <li><i className="fa fa-clock-o"></i>{value.updated_at}</li>
+                                <li><i className="fa fa-calendar"></i>{value.updated_at}</li>
+                            </ul>
+                            <p>{value.comment}</p>
+                            <button className="btn btn-primary" href=""><i className="fa fa-reply"></i>Replay</button>
+                        </div>
+                    </li>
+                );
+            });
+        }
+    };
+
     return (
         <>
-            <BlogDetailComment listComments={listComments}/>
-            <div class="replay-box">
-                <div class="row">
-                    <div class="col-sm-12">
+            <div className="response-area">
+                <ul className="media-list">
+                    {renderComments()}
+                </ul>
+            </div>
+            <div className="replay-box">
+                <div className="row">
+                    <div className="col-sm-12">
                         <h2>Leave a replay</h2>
-
-                        <div class="text-area">
+                        <div className="text-area">
                             <Validate errors={errors} />
                             <form onSubmit={(event) => { handleSubmit(event) }}>
                                 <textarea rows="11" value={comment} onChange={(event) => { handleComment(event) }}></textarea>
-                                <button class="btn btn-primary">post comment</button>
+                                <button className="btn btn-primary">post comment</button>
                             </form>
                         </div>
                     </div>
